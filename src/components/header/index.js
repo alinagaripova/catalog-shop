2fe690166058de5784a0be92d2454c39161c6935
Алина Гайнанова <alinagainanova@MacBook-Air-Alina.local>
2023-01-27 19:48:43 +0300

    updatebtn

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { userSlice } from '../../store/reducers/userData';
@@ -11,17 +11,18 @@ import logo from '../../images/logo.png';
 import './style.scss';
 
 export default function Header(){
+  const [error, setError] = useState(false);
   const { addData, toogleLoading } = userSlice.actions;
   const dispasth = useDispatch();
   const userData = useSelector(state => state.userReducer.userData)
   const isLoading = useSelector(state => state.userReducer.isLoading)
 
-  window.ethereum.on('accountsChanged', (e) => {
+  window?.ethereum?.on('accountsChanged', (e) => {
     dispasth(addData(e[0]))
   });
 
   function connectWallet(){
-    if (userData) return;
+    if (userData || error) return;
     dispasth(toogleLoading(true));
 
     setTimeout(()=>{ // timeOut для наглядности лоадера
@@ -30,9 +31,22 @@ export default function Header(){
         dispasth(addData(data));
         dispasth(toogleLoading(false));
       })
+        .catch(error => {
+          setError(true);
+          dispasth(toogleLoading(false));
+        })
     }, 2000)
   }
 
+  let btnText;
+
+  if(userData){
+    btnText = cutKey(userData);
+  } else if(error) {
+    btnText = "Скачайте MetaMask"
+  } else {
+    btnText = "Connect Wallet"
+  }
 
   return (
     <header className='header'>
@@ -43,7 +57,7 @@ export default function Header(){
       <div className='header__right-block'>
         {isLoading ? 
           <Loader /> :
-          <Button handler={connectWallet} text={userData ? cutKey(userData) : "Connect Wallet"} showLogo={userData} />}
+          <Button handler={connectWallet} text={btnText} showLogo={userData} />}
       </div>
     </header>
   )
